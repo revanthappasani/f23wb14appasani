dc830c1f287c6c74cdd250689dd084a5aa840b45
revanthappasani <143126804+revanthappasani@users.noreply.github.com>
2023-10-31 21:30:21 -0500

    Added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var DeerRouter = require('./routes/Deer');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Deer', DeerRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
