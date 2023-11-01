3d776e894c93571589009e20e3685662f7b63b32
revanthappasani <143126804+revanthappasani@users.noreply.github.com>
2023-10-31 21:15:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var DeerRouter = require('./routes/Deer');
+var boardRouter = require('./routes/board');
+
+
 
 var app = express();
 
@@ -23,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Deer', DeerRouter);
+app.use('/board', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
